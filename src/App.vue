dd4b8e3275f0dcd8b4ed8176b3ff79b36321b68c
vintagehsieh <vintagehsieh@gmail.com>
2023-02-20 13:35:35 +0800

    establish project and index

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Header />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  // 改字體
+  font-family: "Microsoft JhengHei", "Heiti TC", "sans-serif";
+}
+img {
+  display: block;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
 }
 
-nav {
-  padding: 30px;
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// nav {
+//   padding: 30px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<script>
+export default {};
+</script>
+<template>
+  <header>
+    <nav>
+      <!-- logo -->
+      <div id="logo"></div>
+      <!-- Direct -->
+      <div id="pages">
+        <a href="" id="musicPage">音樂播放</a>
+        <a href="" id="shopPage">音樂商城</a>
+        <a href="" id="actPage">音樂活動</a>
+      </div>
+      <!-- membership -->
+      <div id="login">
+        <div id="avatar"></div>
+        <a href="">會員登入</a>
+        <!-- 引入font-awesome -->
+        <!-- <font-awesome-icon icon="fa-solid fa-caret-down" /> -->
+      </div>
+    </nav>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  background-color: #1f2124;
+  width: 100%;
+  height: 100px;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  > #logo {
+    margin-left: 10rem;
+    width: 200px;
+    height: 60px;
+    background-color: white;
+  }
+  > #pages {
+    display: flex;
+    color: white;
+    > a {
+      text-decoration: none;
+      color: white;
+      padding-right: 6rem;
+    }
+  }
+  > #login {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding-right: 10rem;
+    > #avatar {
+      display: block;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      background-color: white;
+    }
+    > a {
+      text-decoration: none;
+      color: white;
+      padding-left: 1rem;
+    }
+  }
+}
+font-awesome-icon {
+  color: white;
+}
+</style>
