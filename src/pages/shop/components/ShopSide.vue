589b3c66d0e2f83f6a5fd81ca10066549f5e8ced
vintagehsieh <vintagehsieh@gmail.com>
2023-03-15 20:34:44 +0800

    shop style fix

--- src/pages/shop/components/ShopSide.vue
+++ src/pages/shop/components/ShopSide.vue
@@ -2,127 +2,125 @@
 import { ref, onMounted } from "vue";
 import Cookies from "js-cookie";
 export default {
-  setup() {
-    const username = ref("");
+    setup() {
+        const username = ref("");
 
-    onMounted(() => {
-      username.value = Cookies.get("loginInfo");
-    });
+        onMounted(() => {
+            username.value = Cookies.get("UserID");
+        });
 
-    return { username };
-  },
+        return { username };
+    },
 };
 </script>
 <template>
-  <div class="content">
-    <div class="navbar">
-      <p>
-        嗨，<span style="color: orange">{{ username }}</span
-        ><br />
-        今天想買什麼?
-      </p>
-      <div class="logo">
-        <img
-          src="@/assets/music-note-icon-song-melody-tune-flat-symbol-free-vector.webp"
-          alt=""
-        />
-      </div>
-      <div class="link">
-        <router-link to="/">
-          <span class="icon"><i class="fa-solid fa-home"></i></span>
-          <h3>商品首頁</h3>
-        </router-link>
-        <router-link to="/shopSearch">
-          <span class="icon"><i class="fa-solid fa-magnifying-glass"></i></span>
-          <h3>商品搜尋</h3>
-        </router-link>
-        <router-link to="/cart">
-          <span class="icon"><i class="fa-solid fa-shopping-cart"></i></span>
-          <h3>購物車</h3>
-        </router-link>
-      </div>
+    <div class="content">
+        <div class="navbar">
+            <p>
+                嗨，<span style="color: orange">{{ username }}</span
+                ><br />
+                今天想買什麼?
+            </p>
+            <div class="logo">
+                <img
+                    src="@/assets/music-note-icon-song-melody-tune-flat-symbol-free-vector.webp"
+                    alt=""
+                />
+            </div>
+            <div class="link">
+                <router-link to="/">
+                    <span class="icon"><i class="fa-solid fa-home"></i></span>
+                    <h3>商品首頁</h3>
+                </router-link>
+                <router-link to="/cart">
+                    <span class="icon"
+                        ><i class="fa-solid fa-shopping-cart"></i
+                    ></span>
+                    <h3>購物車</h3>
+                </router-link>
+            </div>
+        </div>
+        <div class="view">
+            <router-view></router-view>
+        </div>
     </div>
-    <div class="view">
-      <router-view></router-view>
-    </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
 .content {
-  display: flex;
-  flex: 1;
-  width: 100%;
-  height: 100%;
-  .navbar {
     display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
-    background: linear-gradient(
-      180deg,
-      rgba(60, 61, 62, 1) 0%,
-      rgba(59, 55, 49, 1) 100%
-    );
-    opacity: 87%;
-    min-width: 20vw;
-    height: 100%;
-    padding-top: 6rem;
-    color: white;
-    padding-inline: 2rem;
-    p {
-      color: white;
-      font-size: 1.25rem;
-      margin-bottom: 1.75rem;
-      letter-spacing: 4px;
-      text-align: center;
-    }
-    .logo {
-      position: relative;
-      width: 175px;
-      height: 175px;
-      overflow: hidden;
-      border-radius: 50%;
-      img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        scale: 1.2;
-        object-fit: cover;
-      }
-    }
-    .link {
-      margin-top: 4rem;
-      margin-left: -10px;
-      a {
-        display: flex;
-        align-items: center;
-        text-decoration: none;
-        color: white;
-        margin-bottom: 2.5rem;
-        font-size: 1rem;
-        text-align: justify;
-        &:hover {
-          color: orange;
-        }
-        span {
-        }
-        h3 {
-          padding-left: 1rem;
-        }
-      }
-    }
-  }
-  .view {
     flex: 1;
-    .router-link {
-      a {
-        .icon {
+    width: 100%;
+    height: 100%;
+    .navbar {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+        background: linear-gradient(
+            180deg,
+            rgba(60, 61, 62, 1) 0%,
+            rgba(59, 55, 49, 1) 100%
+        );
+        opacity: 87%;
+        min-width: 20vw;
+        height: 100%;
+        padding-top: 6rem;
+        color: white;
+        padding-inline: 2rem;
+        p {
+            color: white;
+            font-size: 1.25rem;
+            margin-bottom: 1.75rem;
+            letter-spacing: 4px;
+            text-align: center;
         }
-        h3 {
+        .logo {
+            position: relative;
+            width: 175px;
+            height: 175px;
+            overflow: hidden;
+            border-radius: 50%;
+            img {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                scale: 1.2;
+                object-fit: cover;
+            }
         }
-      }
+        .link {
+            margin-top: 4rem;
+            margin-left: -10px;
+            a {
+                display: flex;
+                align-items: center;
+                text-decoration: none;
+                color: white;
+                margin-bottom: 2.5rem;
+                font-size: 1rem;
+                text-align: justify;
+                &:hover {
+                    color: orange;
+                }
+                span {
+                }
+                h3 {
+                    padding-left: 1rem;
+                }
+            }
+        }
     }
-  }
+    .view {
+        flex: 1;
+        .router-link {
+            a {
+                .icon {
+                }
+                h3 {
+                }
+            }
+        }
+    }
 }
 </style>
--- src/router/shopRouter.js
+++ src/router/shopRouter.js
@@ -1,44 +1,34 @@
 import {
-  createRouter,
-  createWebHistory,
-  createWebHashHistory,
+    createRouter,
+    createWebHistory,
+    createWebHashHistory,
 } from "vue-router";
 import ShopHome from "../pages/shop/views/shopHome.vue";
 // import ProductItem from "@/pages/shop/views/productItem.vue";
 import Cart from "@/pages/shop/views/cart.vue";
 import Checkout from "@/pages/shop/views/checkout.vue";
-import ShopSearch from "@/pages/shop/views/shopSearch.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "shop",
-    component: ShopHome,
-  },
-  // {
-  //   path: "/productItem/:id",
-  //   name: "productItem_id",
-  //   component: ProductItem,
-  // },
-  {
-    path: "/cart",
-    component: Cart,
-  },
-  {
-    path: "/checkout",
-    component: Checkout,
-  },
-  {
-    path: "/shopSearch",
-    component: ShopSearch,
-  },
+    {
+        path: "/",
+        name: "shop",
+        component: ShopHome,
+    },
+    {
+        path: "/cart",
+        component: Cart,
+    },
+    {
+        path: "/checkout",
+        component: Checkout,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+    history: createWebHashHistory(),
 
-  // history: createWebHistory(process.env.BASE_URL),
-  routes,
+    // history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
