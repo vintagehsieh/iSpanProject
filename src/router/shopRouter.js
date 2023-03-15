61a7cafec006ef2ceb3cf9fa9fd8ebcbdb66723b
Hung08 <sun60703@gmail.com>
2023-03-15 21:02:07 +0800

    fixfix

--- src/router/shopRouter.js
+++ src/router/shopRouter.js
@@ -7,12 +7,18 @@ import ShopHome from "../pages/shop/views/shopHome.vue";
 // import ProductItem from "@/pages/shop/views/productItem.vue";
 import Cart from "@/pages/shop/views/cart.vue";
 import Checkout from "@/pages/shop/views/checkout.vue";
+import ProductItem from "@/pages/shop/views/productItem.vue"
 
 const routes = [
     {
         path: "/",
         name: "shop",
         component: ShopHome,
+    },
+    {
+        path: "/productItem/:id",
+        name: "prodcut_id",
+        component: ProductItem,
     },
     {
         path: "/cart",
